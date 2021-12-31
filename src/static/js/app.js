6bf4d3f8df8e775a06fc6ff8d0dfeb25b52af325
tomoaki <zenizeni0918@yahoo.co.jp>
2021-12-31 18:15:36 +0900

    v1

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -2,6 +2,7 @@ function App() {
     const { Container, Row, Col } = ReactBootstrap;
     return (
         <Container>
+             <div>todo list</div> 
             <Row>
                 <Col md={{ offset: 3, span: 6 }}>
                     <TodoListCard />
